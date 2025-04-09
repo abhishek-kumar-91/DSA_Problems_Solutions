3f0fc53530c19e424474efc25fa95c16891e2425
Abhishek Kumar <abhishekkumar958830@gmail.com>
2025-04-09 14:18:49 +0530

    solved basic javascript problems

---
+++ Day 5/javaScriptBasicQue.js
@@ -0,0 +1,22 @@
+const product = {
+    productName: "Parker jotter standard CT Ball Pen",
+    startPoint: 4,
+    deals: "Deal of the day",
+    price: 270
+}
+
+console.log(product)
+
+const profile = {
+    fullName: "Abhishek Kumar",
+    isFollow: true,
+    isFollowing: false,
+    posts: 149,
+    followers: 158,
+    following: 6,
+    userID: "@abhishekkumar",
+    passion: "Enterprenure",
+    description: "alt founder, tech enthusiasts"
+}
+
+console.log(profile);
