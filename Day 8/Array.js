1636c86531fa6e4d88f4d3e4c5380d47347dc458
Abhishek Kumar <abhishekkumar958830@gmail.com>
2025-04-17 10:52:10 +0530

    day 8 Solved easy array ques

---
+++ Day 8/Array.js
@@ -0,0 +1,48 @@
+let arr = [1,2,3,4,5,6];
+// console.log("Before adding the elements", arr.length)
+// arr[arr.length ] = 76;
+// arr.push(99);
+// console.log("After adding the elements", arr.length)
+
+// arr.shift(); // remove first element from the array
+// console.log(arr);
+
+// console.log(arr.length); //finding length of the array
+
+// console.log(arr[2]); //access the third item from the array
+
+// console.log(typeof arr);
+// const strarr = arr.toString();
+// console.log(typeof strarr, strarr)
+
+// console.log(arr.includes(100))
+
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = i; j < arr.length; j++){
+//         if(arr[j] < arr[i]){
+//             let temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+//     }
+// }
+
+// console.log(arr)
+
+// arr.reverse()
+
+// for(let i = 0; i < arr.length/2; i++){
+//     let j = arr.length - 1 - i; // Index from the end
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+// }
+// console.log(arr)
+
+// let arr2 = [5,5,454,54,5,45,4,4,4];
+// let mergedarr= arr.concat(arr2);
+// // console.log(mergedarr);
+
+// mergedarr.forEach((val, i) => {
+//     console.log(val, i);
+// })
